fe59de68b43c967ae73acb5f43a10df7247b7685
Jaiprakashagent1 <jaiprakash.v953@gmail.com>
2026-08-09 20:40:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,4 +48,3 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
-
